ee6036b39639c2dfd27282ec69e553f7b464fb5d
Abhishek Samariya <asamariya@gmail.com>
2019-08-22 15:20:57 +1000

    back end done, need a front end dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts "Starting to creat cocktails"
+
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "mint leaves")
+Ingredient.create!(name: "vodka")
+Ingredient.create!(name: "whiskey")
+Ingredient.create!(name: "milk")
+
+
+Cocktail.create!(name: "Mint Julep", )
+Cocktail.create!(name: "Whiskey Sour")
+Cocktail.create!(name: "Mojito")
+Cocktail.create!(name: "Negroni")
+
+# byebug
+Dose.create!(description: "2cl", cocktail_id: Cocktail.first.id, ingredient_id: Ingredient.first.id)
+Dose.create!(description: "2cl", cocktail_id: Cocktail.first.id, ingredient_id: Ingredient.last.id)
+Dose.create!(description: "3cl", cocktail_id: Cocktail.find_by(name: "Whiskey Sour").id, ingredient_id: Ingredient.find_by(name: 'mint leaves').id)
+Dose.create!(description: "6cl", cocktail_id: Cocktail.last.id, ingredient_id: Ingredient.last.id)
+Dose.create!(description: "9cl", cocktail_id: Cocktail.find_by(name: "Mojito").id, ingredient_id: Ingredient.find_by(name: "ice").id)
+
+puts "Finished, enjoy! #{Ingredient.count}, #{Cocktail.count}, #{Dose.count}"
